6ceb4113da0c53f59b2010ab07efed84688ffbcd
uglymunky <gabriel@uglymunky>
2012-03-18 14:34:23 -0700

    - woohoo!!  Finally got hogan + partials working with Express!

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
 // Use express
 var express = require('express')
+, hogan = require('hogan.js')
 
 // Instantiate our app/server
 , app = module.exports = express.createServer();
@@ -13,12 +14,27 @@ app.configure(function(){
 	app.set('views', __dirname + '/views');
 
 	// Set '.hogan' as the default extension
-	app.set('view engine', 'jade');
+	app.set('view engine', 'hogan');
 
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
 	app.use(app.router);
 	app.use(express.static(__dirname + '/public'));
+
+
+	app.register('hogan', {
+		compile: function (source) {
+		    return function (options) {
+			    var partials = options.partials || {};
+
+			    if (options.body) {
+				    partials.body = options.body;
+			    }
+
+		        return hogan.compile(source, options).render(options, partials);
+		    };
+		}
+	});
 });
 
 app.configure('development', function(){
--- routes/main.js
+++ routes/main.js
@@ -1,3 +1,46 @@
+var fs = require('fs')
+, viewData = {
+	title: 'dataviz'
+	, navItems: [
+		{
+			name: 'borrowers'
+			, text: 'borrowers'
+		}
+		, {
+			name: 'lenders'
+			, text: 'lenders'
+		}
+		, {
+			name: 'site'
+			, text: 'web site'
+		}
+		, {
+			name: 'partners'
+			, text: 'partners'
+		}
+		, {
+			name: 'ecosystem'
+			, text: 'kiva ecosystem'
+		}
+		, {
+			name: 'stories'
+			, text: 'stories from the field'
+		}
+		, {
+			name: 'press'
+			, text: 'press & promotions'
+		}
+		, {
+			name: 'fundraising'
+			, text: 'fundraising'
+		}
+	]
+	, partials: {
+		sidebar: fs.readFileSync('/localhost/dataviz/views/includes/sidebar.hogan', 'utf8')
+	}
+}
+
+
 module.exports =  function (req, res) {
-	res.render('index', {title: 'dataviz'});
+	res.render('index', viewData);
 };
