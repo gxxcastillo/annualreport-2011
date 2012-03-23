02c7052347086f96f80235ba2e8829787bc029e6
uglymunky <gabriel@uglymunky>
2012-03-23 11:06:34 -0700

    - add a set of new router functions that request multiple sections at once

--- public/js/router.js
+++ public/js/router.js
@@ -1,5 +1,22 @@
 define(['jquery', 'backbone', 'dv'], function ($, Backbone, dv) {
 
+	// Store the current section
+	// @todo, make currentSection non-static
+	var currentSection = ''
+
+	// @todo, this is also set on the server side, should only be set in one spot
+	, sections = [
+		'borrowers'
+		, 'lenders'
+		, 'site'
+		, 'partners'
+		, 'ecosystem'
+		, 'stories'
+		, 'press'
+		, 'fundraising'
+	];
+
+
 	/**
 	 * Backbone.Router provides methods for routing client-side pages, and connecting them to actions and events.
 	 * router.navigate does not support the history API's stateObject
@@ -12,17 +29,31 @@ define(['jquery', 'backbone', 'dv'], function ($, Backbone, dv) {
 
 
 		/**
+		 * Getter & setter for the currentSection
+		 */
+		, activeSection: function (section) {
+			if (section) {
+				return currentSection = section;
+			}
+
+			return currentSection;
+		}
+
+
+		/**
 		 * The route for navigating to a section.
 		 *
 		 * @todo. pass param for scrolling?
 		 */
 		, showSection: function (section) {
+			this.activeSection(section);
 			this.getSection(section);
 		}
 
 
 		/**
 		 * @todo this should probably go in some dv ajax module
+		 * @todo what should this url look like?  What should it look like if requesting multiple sections?
 		 *
 		 * Does an ajax request for a section.
 		 */
@@ -36,6 +67,79 @@ define(['jquery', 'backbone', 'dv'], function ($, Backbone, dv) {
 		}
 
 
+		/**
+		 * Get the next section
+		 *
+		 */
+		, getNextSection: function (callback) {
+			var i = _.indexOf(sections, currentSection);
+			this.getSection(sections[i+1]);
+
+			callback();
+		}
+
+
+		/**
+		 * Get all sections up to the section provided
+		 *
+		 * @params {String} from
+		 * @params {String} [to]
+		 *
+		 * @todo request an array of each element I am requesting
+		 *
+		 */
+		, getSectionsUpFrom: function (from, to) {
+			to = to || currentSection;
+
+			var fromIndex = _.indexOf(sections, from)
+			, toIndex = _.indexOf(sections, to)
+			, sectionsToRequest;
+
+			// @todo add some kind of messaging?
+			if (fromIndex + toIndex < 0) {
+				return;
+			}
+
+			sectionsToRequest = sections.splice(fromIndex, toIndex);
+
+			if (!_.isArray(sectionsToRequest)) {
+				return;
+			}
+
+			this.getSections(sectionsToRequest);
+
+		}
+
+
+		/**
+		 * @todo can this be combined with getSectionsUpFrom
+		 * Get all sections that come up after the section provided
+		 *
+		 * @params {String} to
+		 * @params {String} [from]
+		 */
+		, getSectionsUpTo: function (to, from) {
+			from = from || currentSection;
+
+			var toIndex = _.indexOf(sections, to)
+			, fromIndex = _.indexOf(sections, from)
+			, sectionsToRequest;
+
+			// @todo add some kind of messaging
+			if (toIndex + fromIndex < 0) {
+				return;
+			}
+
+			sectionsToRequest = sections.splice(toIndex, fromIndex);
+
+			if (!_.isArray(sectionsToRequest)) {
+				return;
+			}
+
+			this.getSections(sectionsToRequest);
+		}
+
+
 		, defaultAction: function () {
 			console.log('no route');
 			this.navigate('borrowers', {trigger: true});
@@ -44,7 +148,7 @@ define(['jquery', 'backbone', 'dv'], function ($, Backbone, dv) {
 
 		, initialize: function () {
 			// Wait for domReady, IE history fallback relies on an iframe.
-			// @todo use Modernizr to detct history support, if has native history support, no need to wait for domready
+			// @todo use Modernizr to detect history support, if has native history support, no need to wait for domready
 			$(Backbone.history.start({pushState: true /*, silent: true */}));
 		}
 	});
