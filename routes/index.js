11794cd795837b791a88a72e98161292320b6ecb
uglymunky <gabriel@uglymunky>
2012-03-23 18:29:18 -0700

    Add next / prev links

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,8 @@
 // @todo this probably needs to go into a model somewhere
 var viewData = {
 	title: 'dataviz'
-	, navItems: [
+	, navMenuItems: [
 		{
-			name: 'prev'
-			, text: 'prev'
-		}
-		, {
-			name: 'next'
-			, text: 'next'
-		}
-		, {
 			name: 'borrowers'
 			, text: 'borrowers'
 		}
@@ -19,12 +11,12 @@ var viewData = {
 			, text: 'lenders'
 		}
 		, {
-			name: 'site'
-			, text: 'web site'
-		}
-		, {
 			name: 'partners'
 			, text: 'partners'
+		}
+		, {
+			name: 'site'
+			, text: 'web site'
 		}
 		, {
 			name: 'ecosystem'
@@ -39,12 +31,28 @@ var viewData = {
 			, text: 'press & promotions'
 		}
 		, {
-			name: 'fundraising'
-			, text: 'fundraising'
+			name: 'finances'
+			, text: 'financial health'
 		}
 	]
 };
 
+// @contrived...but then, so is this whole project
+// @todo these are the menu items, in order: var navItems = ['borrowers', 'lenders', 'site', 'partners', 'ecosystem', 'stories', 'press', 'fundraising'];
+
+// @todo This mapping get produced dynamically
+var navItemMapping = {
+	borrowers: 1
+	, lender: 2
+	, site: 3
+	, partners: 4
+	, ecosystem: 5
+	, stories: 6
+	, press: 7
+	, fundraising: 8
+}
+
+viewData.navItemMapping = navItemMapping;
 
 module.exports =  function (req, res) {
 	res.render('index', viewData);
