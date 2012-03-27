a42fdf5e9ccf8ff847797bfc16fc5e2420525267
uglymunky <gabriel@uglymunky>
2012-03-26 23:17:13 -0700

    some clean up for the section model

--- public/js/models/Section.js
+++ public/js/models/Section.js
@@ -5,14 +5,28 @@ define(['jquery', 'underscore', 'backbone', 'dv', 'Block'], function ($, _, Back
 		defaults: {
 			isActive: false
 			, isRendered: false
+			, isLoaded: false
 		}
 
 		, initialize: function (sectionData) {
 			var blocksArray = sectionData.blocks
 			, blocks = [];
 
+			// Build an array of all the blocks for this section
 			_.each(blocksArray, function (blockData, index) {
 				blocks[index] = new Block(blockData);
+			});
+
+			// Only initialze the "blocks" attribute if we have blocks to initialize it with
+			if (blocks.length) {
+				this.set('blocks', blocks);
+			}
+
+			// Automatically update the "isLoaded" attribute when blocks are set
+			this.on('change:blocks', function (model, newBlocks) {
+				if (!this.get('isLoaded') && blocks.length) {
+					this.set('isLoaded', true);
+				}
 			});
 		}
 	});
