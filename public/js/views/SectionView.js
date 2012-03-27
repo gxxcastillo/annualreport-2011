f067dc1a1dd618b63f095436110e73e2d5c48317
uglymunky <gabriel@uglymunky>
2012-03-27 01:01:30 -0700

    Bind the sectionView to the section Model + cleanup

--- public/js/views/SectionView.js
+++ public/js/views/SectionView.js
@@ -6,50 +6,34 @@ define(['jquery', 'underscore', 'backbone', 'dv', 'hogan', 'BlockView']
 		tagName: 'section'
 
 
-		, template: function (data) {
-			if (! this.tpl) {
-				this.tpl = hogan.compile($('#sectionTemplate').html())
-			}
-
-			return this.tpl.render(data);
-		}
-
-
 		/**
 		 * Render the section
-		 *
-		 * @params {Object} viewData
 		 */
-		, render: function (viewData) {
-			var $section = this.$el;
-
-			// Build the section element
-			var l = $section
+		, render: function () {
+			var viewData = this.model.toJSON()
+			, $section = this.$el
 				.attr('id', viewData.id)
-
-			$section.html(new BlockView({
+				.html(new BlockView({
 					name: 'sectionTitle'
 					, title: viewData.title
 					, cssClass: 'g2 h1'
 				}).$el);
 
-			// Append each block
+
+			// Each section has multiple "blocks", append each one to the section's DOM
 			_.each(viewData.blocks, function (block) {
 				var newBlock = new BlockView(block);
 
 				$section.append(newBlock.el);
 			});
 
-			dv.trigger('render.sectionView', [viewData.id]);
+			// Update the model's state
+			this.model.set('isRendered', true);
 		}
 
 
-		, initialize: function (viewData) {
-			this.name = viewData.name;
-
-			if (viewData) {
-				this.render(viewData);
-			}
+		, initialize: function () {
+			this.render();
 		}
 	});
 });
