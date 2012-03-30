839cec5cf383186c6a66a1e02683bbd11d7ade89
uglymunky <gabriel@uglymunky>
2012-03-29 21:11:52 -0700

    document + cleanup

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,31 @@
 require.config({
     baseUrl: '/js'
 	, paths: {
-		'order': 'lib/order'
-		, 'text': 'lib/text'
+		// 3rd party dependancies
+		'order': 'lib/order'                        // For manually specifying loading order.  http://requirejs.org/docs/api.html#order
+		, 'text': 'lib/text'                        // For loading text files. http://requirejs.org/docs/api.html#text
 		, 'jquery': 'lib/jquery'
-		, 'underscore': 'lib/underscore'
-		, 'backbone': 'lib/backbone'
+		, 'underscore': 'lib/underscore'            // Functional library. http://documentcloud.github.com/underscore/
+		, 'backbone': 'lib/backbone'                // MVC framework. http://documentcloud.github.com/backbone/
+		, 'hogan': 'lib/hogan'                      // Templating engine. http://twitter.github.com/hogan.js/
 		, 'hogan.template': 'lib/hogan/template'
 		, 'hogan.compiler': 'lib/hogan/compiler'
-		, 'hogan': 'lib/hogan'
-		, 'jquery.isotope': 'lib/jquery.isotope'
-		, 'jquery.masonry': 'lib/jquery.masonry'
-		, 'jquery.colorbox': 'lib/jquery.colorbox'
-		, 'jquery.infinitescroll': 'lib/jquery.infinitescroll'
-		, 'jquery.waypoints': 'lib/jquery.waypoints'
+		, 'jquery.isotope': 'lib/jquery.isotope'    // Dynamic positioning of elements. http://isotope.metafizzy.co
+		, 'jquery.colorbox': 'lib/jquery.colorbox'  // Lightboxes. http://www.jacklmoore.com/colorbox
+		, 'jquery.waypoints': 'lib/jquery.waypoints'// Trigger scrolling events. http://imakewebthings.com/jquery-waypoints/
+
+		// The annual report app
+
+		// Models
 		, 'AnnualReport': 'models/AnnualReport'
 		, 'Section': 'models/Section'
 		, 'Block': 'models/Block'
 		, 'NavItem': 'models/NavItem'
+
+		// Collections
 		, 'Sections': 'collections/Sections'
+
+		// Views
 		, 'LayoutView': 'views/LayoutView'
 		, 'SidebarView': 'views/SidebarView'
 		, 'MainView': 'views/MainView'
@@ -30,14 +37,22 @@ require.config({
 require(['underscore', 'backbone', 'dv', 'Router', 'AnnualReport', 'LayoutView', 'order!jquery', 'order!jquery.isotope', 'order!jquery.waypoints', 'order!jquery.colorbox']
 , function (_, Backbone, dv, Router, AnnualReport, LayoutView) {
 
-	var annualReport = new AnnualReport
+	var
+	// Model
+	annualReport = new AnnualReport
+
+	// View
 	, layoutView = new LayoutView({annualReport: annualReport})
+
+	// Controller
 	, router = new Router({annualReport: annualReport, layoutView: layoutView});
 
-	// @todo Here for testing
+	/*
+	// For testing only
 	window.dvTesting = {
 		annualReport: annualReport
 		, router: router
 		, layoutView: layoutView
 	};
+	*/
 });
