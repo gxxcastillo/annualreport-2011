1500c2126fd66537edb71de96639b06f95856702
uglymunky <gabriel@uglymunky.com>
2012-04-10 14:22:26 -0700

    add new files

---
+++ public/js/views/BlockView.SectionTitle.js
@@ -0,0 +1,10 @@
+// @TODO This was the beginnings of an inheritance pattern for block views...putting it off for now...
+
+define(['jquery', 'underscore', 'backbone', 'dv', 'SidebarView', 'BlockView']
+, function ($, _, Backbone, dv, SidebarView, BlockView) {
+
+	return BlockView.extend({
+
+	});
+
+});
