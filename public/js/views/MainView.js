7431101f6a9c8b2596fc257bb484d959fa7b5ac0
uglymunky <gabriel@uglymunky.com>
2012-04-28 12:15:28 -0700

    cache  the scrollTop - scroll events are very sluggish, I was hoping that caching the scrollTop value would fix that but I'm not so sure that it did. - overall, I'm sure that avoiding the redundant DOM calls is a good thing

--- public/js/views/MainView.js
+++ public/js/views/MainView.js
@@ -55,27 +55,32 @@ define(['jquery', 'underscore', 'backbone', 'dv', 'SectionView']
 		 * @params {String} sectionId
 		 */
 		, scrollTo: function (sectionId) {
-			var model = this.model
-			, $sectionTitleBlock = $('#' + sectionId + ' .sectionTitleBlock')
-			, scrollTop = $sectionTitleBlock.data('isotope-item-position').y;
+			var mainView = this
+
+			, model = this.model
+
+			, scrollTop = this.scrollTopCache[sectionId];
+
+			// Since sections have no height, 'isotope' is the most reliable way to find the 'scrollTop'
+			// However, getting this value is slow and requires caching for smooth interaction
+			if (scrollTop === undefined) {
+				scrollTop = this.scrollTopCache[sectionId] = $('#' + sectionId + ' .sectionTitleBlock').data('isotope-item-position').y;
+			}
 
 			model.set('blockWaypointActivation', true);
 
-			// We have to use the top position of the sectionTitle
-			// because section position values are not reliable when using jquery.isotope (sections have no height).
-			$('html, body').stop().animate({scrollTop: scrollTop}, function() {
+			this.$htmlBody.stop().animate({scrollTop: scrollTop}, function() {
 
-				// Add a slight delay, then re-adjust the scrollTop and un-blocking the waypoints
-				// We re-scroll in cases where the initial scroll position has changed since the initial render (due to animations)
-				// Immediately unblocking the waypoints seems to be too early and results in collisions with other navigation events
+				// On initial load, scrolling past more than ~3 sections results in scrollTop being > than the height of the document
+				// So, we wait, and assume the document will have reached the correct height on a second pass.
 				window.setTimeout(function () {
-					if (scrollTop != $sectionTitleBlock.data('isotope-item-position').y) {
-						$('html, body').stop().animate({scrollTop: $sectionTitleBlock.data('isotope-item-position').y});
+					if (mainView.$document.scrollTop() < scrollTop) {
+						mainView.$htmlBody.stop().animate({scrollTop: scrollTop});
 					}
 
 					model.set('blockWaypointActivation', false);
 				}, 100);
-			} );
+			});
 		}
 
 
@@ -87,6 +92,12 @@ define(['jquery', 'underscore', 'backbone', 'dv', 'SectionView']
 			this.sections = this.model.get('sections');
 			this.renderAll = this.model.get('renderAll');
 			this.animate = this.model.get('animate');
+
+			this.scrollTopCache = [];
+
+			// @todo, this is the best crossbrowser way of scrolling, however, it causes the scroll event to fire twice
+			this.$htmlBody = $('html, body');
+			this.$document = $(document);
 
 			// Enable jquery.masonry?
 			if (this.animate) {
