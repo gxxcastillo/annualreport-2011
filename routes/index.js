f786311736604c43d8117120141656098c5e0d1b
gabriel <gabriel@kiva.org>
2012-05-06 11:11:45 -0700

    drop the url for now, it gets added by the js later anyway

--- routes/index.js
+++ routes/index.js
@@ -17,11 +17,5 @@ module.exports =  function (req, res) {
     sectionList[0].first = true;
     layoutData.sectionList = sectionList;
 
-	if (process.env.NODE_ENV == 'production') {
-		layoutData.fbShareUrl = 'http://www.facebook.com/dialog/feed?app_id=372116016173485&link=http://annualreport.kiva.org&redirect_uri=http://annualreport.kiva.org';
-	} else {
-		layoutData.fbShareUrl = 'http://www.facebook.com/dialog/feed?app_id=348783781832034&link=http://annualreport-vm.kiva.org&redirect_uri=http://annualreport-vm.kiva.org';
-	}
-
     res.render('index', layoutData);
 }
